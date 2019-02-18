44561a92b8c88e07494fb99a0ea24b1676d9091e
Christopher McIntosh <christopher.l.mcintosh@outlook.com>
2019-02-18 21:02:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ var recipes = {}
 function updateObjectWithKeyAndValue(recipes, key, value) {
   return Object.assign({}, recipes, {[key]: value})
 }
+function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value) {
+  
+}
